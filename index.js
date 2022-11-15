624a87eff8131ccc24508c2d60fd10cffab8319c
gonzalo <gonzalogarciadev@gmail.com>
2022-11-15 13:12:18 -0500

    Phonebook backend 3.11

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from "cors"
 const app = express();
 
 app.use(cors())
-app.use(express.json());
+app.use(express.static('build'));
 morgan.token("content", (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(
